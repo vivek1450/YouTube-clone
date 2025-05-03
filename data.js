8a39e16045484a61ffc5d2ba83d6401b82e79115
Vivek kumar mahato <167423741+vivek1450@users.noreply.github.com>
2025-05-03 12:19:10 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,15 @@
+export const API_KEY = 'AIzaSyDOgcORs_TKW7LNqXI51i93pxGy_gD6n6Y';
+
+export const value_converter = (value) => {
+    if(value>=1000000)
+    {
+        return Math.floor(value/1000000) +"M";
+    }
+    else if(value>=1000)
+    {
+        return Math.floor(value/1000)+"K";
+    }
+    else{
+        return value;
+    }
+    }
